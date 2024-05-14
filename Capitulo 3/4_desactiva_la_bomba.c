137bb25eba3dbf67b5d1b8db68a26110b973404c
Juan Francisco Padilla Fuentes <jfpadifu03@gmail.com>
2024-05-14 19:31:39 +0200

    Add new file related with a bomb disposal game

---
+++ Capitulo 3/4_desactiva_la_bomba.c
@@ -0,0 +1,11 @@
+/*
+    CURSO C LIBRO KERNIGHAN & RITCHIE
+    Capítulo 3: Control de flujo.
+    herramientas: funciones, arreglos
+
+    Ejercicio: Desactiva la bomba. Para ello, disponemos de las siguientes características:
+    - Generar un número aleatorio del 1 al 100 (código de la bomba)
+    - Generar 5 números aleatorios del 1 al 10 (claves de resolución)
+    - Emplear operaciones aritméticas con esos 5 números para obtener el código de la bomba.
+    - Opcional: Añadir tiempo con clock() de la librería "time.h"
+*/
