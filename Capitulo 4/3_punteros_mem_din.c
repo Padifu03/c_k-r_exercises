3d4b8f284abb1decfe15037bd50cfc6044ae034f
Juan Francisco Padilla Fuentes <jfpadifu03@gmail.com>
2024-06-26 19:33:01 +0200

    Add exercise 3, it is a copy of K&R pointers library.

---
+++ Capitulo 4/3_punteros_mem_din.c
@@ -0,0 +1,32 @@
+/*
+    CURSO C LIBRO KERNIGHAN & RITCHIE
+    Capítulo 4: Punteros.
+    herramientas: punteros, memoria dinámica.
+    Librería obtenida del libro de K&R.
+*/
+#include "stddef.h"
+
+#define ALLOCSIZE   1000000
+
+static char allocbuf[ALLOCSIZE];
+static char *pallocbuf = allocbuf;
+
+char *alloc(int n)
+{
+    int ret_val = 0;
+    if(allocbuf + ALLOCSIZE - pallocbuf >= n)
+    {
+        pallocbuf += n;
+        ret_val = pallocbuf - n;
+    }
+
+    return ret_val;
+}
+
+void afree(char *pointer)
+{
+    if ((pointer >= allocbuf) && (pointer < (allocbuf + ALLOCSIZE)))
+    {
+        pallocbuf = pointer;
+    }
+}
